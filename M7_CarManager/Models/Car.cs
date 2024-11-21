85731ac57f41afcbf53022c1e11baa73495fcb51
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-11-21 20:05:40 +0200

    Models - Car - Unique Id added

--- M7_CarManager/Models/Car.cs
+++ M7_CarManager/Models/Car.cs
@@ -2,8 +2,14 @@
 {
     public class Car
     {
+        public string Id { get; set; }
         public string Model { get; set; }
         public string PlateNumber { get; set; }
         public int Price { get; set; }
+
+        public Car()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
     }
 }
