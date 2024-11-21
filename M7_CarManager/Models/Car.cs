27260b363f4b4ff3f0d664ef84c8fda2c14f9220
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-11-21 20:01:12 +0200

    Models - Car model added

---
+++ M7_CarManager/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace M7_CarManager.Models
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string PlateNumber { get; set; }
+        public int Price { get; set; }
+    }
+}
