23a129979ec5bc8f954650ac9b02cff99d885f92
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-11-22 13:02:17 +0200

    Endpoint - Car repository added

---
+++ M7_CarManager/Data/CarRepository.cs
@@ -0,0 +1,61 @@
+﻿using M7_CarManager.Models;
+
+namespace M7_CarManager.Data
+{
+    public class CarRepository : ICarRepository
+    {
+        static IList<Car> _cars = new List<Car>()
+        {
+            new Car { Model = "Peugeot 306", PlateNumber = "ABC-123", Price = 4000 },
+            new Car { Model = "Suzuki Swift", PlateNumber = "DCB-321", Price = 1000 },
+            new Car { Model = "Opel Astra", PlateNumber = "DDD-333", Price = 2000 },
+        };
+
+        public CarRepository() { }
+
+        // CRUD
+        public void Create(Car car)
+        {
+            if (car.PlateNumber.Length != 7)
+            {
+                throw new ArgumentException("Platenumber format is invalid");
+            }
+            _cars.Add(car);
+        }
+
+        public IEnumerable<Car> Read()
+        {
+            return _cars;
+        }
+
+        public Car? Read(string id)
+        {
+            return _cars.FirstOrDefault(car => car.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            var oldCar = IsInCars(car.Id);
+            foreach (var prop in typeof(Car).GetProperties().Where(p => p.Name != "Id"))
+            {
+                prop.SetValue(oldCar, prop.GetValue(car));
+            }
+        }
+
+        public void Delete(string id)
+        {
+            var carToDelete = IsInCars(id);
+            _cars.Remove(carToDelete);
+        }
+
+        private Car IsInCars(string id)
+        {
+            var car = _cars.FirstOrDefault(c => c.Id == id);
+            if (car == null)
+            {
+                throw new ArgumentException("No car founded!");
+            }
+            return car;
+        }
+    }
+}
---
+++ M7_CarManager/Data/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using M7_CarManager.Models;
+
+namespace M7_CarManager.Data
+{
+    public interface ICarRepository
+    {
+        void Create(Car car);
+        void Delete(string id);
+        IEnumerable<Car> Read();
+        Car? Read(string id);
+        void Update(Car car);
+    }
+}
