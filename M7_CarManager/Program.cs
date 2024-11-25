c065d473231a7393d7485157b318187900f0aa84
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-11-25 23:02:21 +0200

    Program.cs - Configure database, authentication

--- M7_CarManager/Program.cs
+++ M7_CarManager/Program.cs
@@ -1,6 +1,12 @@
 using M7_CarManager.Data;
 using M7_CarManager.Filters;
 using M7_CarManager.Hubs;
+using M7_CarManager.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +16,43 @@ builder.Services.AddControllers(opt =>
 {
     opt.Filters.Add<ApiExceptionFilter>();
 });
+
+// Add DbContext
+builder.Services.AddDbContext<ApiDbContext>(options =>
+{
+    options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarManagerJWT;Trusted_Connection=True;MultipleActiveResultSets=True");
+});
+
+// Add user and role management
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddEntityFrameworkStores<ApiDbContext>()
+    .AddDefaultTokenProviders();
+
+// configure JWT based authentication
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = "http://www.security.org",
+        ValidIssuer = "http://www.security.org",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("verylongverysecretcodeverylongverysecretcode"))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +69,7 @@ if (app.Environment.IsDevelopment())
 
 app.MapHub<EventHub>("/events");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
